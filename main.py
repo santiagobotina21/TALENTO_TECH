74c5adc2dd56b6f8cceb218ca1d8a23e19decf4c
Santiago <santiagobotina21@gmail.com>
2024-06-22 13:37:56 -0500

    CLASE DE VARIABLES EN PYTHON2

--- main.py
+++ main.py
@@ -19,14 +19,24 @@ else:
 flag=True
 flagF=False
 number=17
-float=1.8
-name="Santiago"
+number2=20
+nota1=1.8
+nota2=3.5
+name="Santiago Esteban"
+last_name="Botina Arciniegas"
 peers=(2,4,6,8,10,11)
+odd=(1,3,5,7,9)
 
 
 print('la variable: ',flag,'es de tipo: ',type(flag))
 print('la variable: ',flagF,'es de tipo: ',type(flagF))
 print('la variable: ',number,'es de tipo: ',type(number))
-print('la variable: ',float,'es de tipo: ',type(float))
+print('la variable: ',number2,'es de tipo: ',type(number2))
+print('la variable: ',nota1,'es de tipo: ',type(nota1))
+print('la variable: ',nota2,'es de tipo: ',type(nota2))
 print('la variable: ',name,'es de tipo: ',type(name))
+print('la variable: ',last_name,'es de tipo: ',type(last_name))
 print('la variable: ',peers,'es de tipo: ',type(peers))
+print('la variable: ',odd,'es de tipo: ',type(odd))
+
+
