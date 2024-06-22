f3c4e969e16d166535cb9afbab5e371392d52530
Santiago <santiagobotina21@gmail.com>
2024-06-22 13:15:28 -0500

    CLASE DE VARIABLES EN PYTHON

---
+++ main.py
@@ -0,0 +1,32 @@
+# COMENTARIO
+print("Hola mundo")
+
+edad=18
+
+#Imprimiendo la variable edad
+print(edad)
+
+edad=input("Ingrese su edad: ")
+
+if int(edad) >=18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+
+## TIPOS DE DATOS
+
+flag=True
+flagF=False
+number=17
+float=1.8
+name="Santiago"
+peers=(2,4,6,8,10,11)
+
+
+print('la variable: ',flag,'es de tipo: ',type(flag))
+print('la variable: ',flagF,'es de tipo: ',type(flagF))
+print('la variable: ',number,'es de tipo: ',type(number))
+print('la variable: ',float,'es de tipo: ',type(float))
+print('la variable: ',name,'es de tipo: ',type(name))
+print('la variable: ',peers,'es de tipo: ',type(peers))
